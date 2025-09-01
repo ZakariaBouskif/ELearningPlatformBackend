b93a8efff41f7f022465bd72805316c92432ef34
ZakariaBouskif <z.bouskif@gmail.com>
2025-09-01 19:33:20 +0100

    First commit !

---
+++ src/main/java/com/elearningplatform/ELearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elearningplatform/ELearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
