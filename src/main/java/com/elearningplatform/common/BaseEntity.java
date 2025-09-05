33058bf3dbb3de6d3bad886f05c6e7866d790ba9
ZakariaBouskif <z.bouskif@gmail.com>
2025-09-05 17:49:12 +0100

    Implement the user entity

---
+++ src/main/java/com/elearningplatform/common/BaseEntity.java
@@ -0,0 +1,51 @@
+package com.elearningplatform.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "last_modified_date", insertable = false)
+	private LocalDateTime lastModifiedDate;
+
+	@CreatedBy
+	@Column(name = "created_by", nullable = false, updatable = false)
+	private Long createdBy;
+
+	@LastModifiedBy
+	@Column(name = "last_modified_by", insertable = false)
+	private String lastModifiedBy;
+}
---
+++ src/main/java/com/elearningplatform/entity/User.java
@@ -0,0 +1,137 @@
+package com.elearningplatform.entity;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.CollectionUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "lastName", nullable = false)
+	private String lastName;
+
+	@Column(name = "email", unique = true, nullable = false)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "date_of_birth")
+	private LocalDateTime dateOfBirth;
+
+	@Column(name = "is_enabled")
+	private boolean enabled;
+
+	@Column(name = "is_account_locked")
+	private boolean locked;
+
+	@Column(name = "is_credentials_expired")
+	private boolean credentialsExpired;
+
+	@Column(name = "is_email_verified")
+	private boolean emailVerified;
+
+	@Column(name = "profile_picture_url")
+	private String profilePictureUrl;
+
+	@Column(name = "is_phone_verified")
+	private boolean phoneVerified;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "last_modified_date", insertable = false)
+	private LocalDateTime lastModifiedDate;
+
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles", joinColumns = { @JoinColumn(name = "users_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "roles_id") })
+	private List<Role> roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (CollectionUtils.isEmpty(roles)) {
+			return List.of();
+		}
+		return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).toList();
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !this.locked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return !this.credentialsExpired;
+	}
+
+	public String getFullName() {
+		return this.firstName + " " + this.lastName;
+	}
+
+}
